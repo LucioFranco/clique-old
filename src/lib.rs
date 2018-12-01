4227edc2327b814a42d6ea87c3106691aa55ebcd
Lucio Franco <luciofranco14@gmail.com>
2018-12-01 12:56:40 -0500

    Start to move to async/await

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,10 @@
 //!
 //! A SWIM based gossip protocol library. Coming soon.
 
-#![warn(rust_2018_compatibility)]
-#![warn(rust_2018_idioms)]
-
-extern crate bytes;
-extern crate futures;
-extern crate indexmap;
-extern crate log;
-extern crate serde_derive;
-extern crate tokio;
-extern crate uuid;
+#![feature(await_macro, async_await, futures_api)]
 
 #[macro_use]
 extern crate prost_derive;
-extern crate tower_grpc;
-extern crate tower_h2;
-extern crate tower_http;
 
 mod broadcasts;
 mod codec;

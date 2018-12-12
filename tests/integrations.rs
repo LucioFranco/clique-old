a398eba5a8cfdff8ae1bcbeb24ff8eff898cca4c
Lucio Franco <luciofranco14@gmail.com>
2018-12-11 22:44:24 -0500

    Add next_addr for tests

--- tests/integrations.rs
+++ tests/integrations.rs
@@ -9,23 +9,30 @@ use {
         future::FutureExt,
         task::SpawnExt,
     },
-    std::{future::Future, sync::Arc},
+    std::{
+        future::Future,
+        net::SocketAddr,
+        sync::{
+            atomic::{AtomicUsize, Ordering},
+            Arc,
+        },
+    },
     tokio_async_await_test::async_current_thread_test,
 };
 
 #[async_current_thread_test]
 async fn join() {
-    let node_a_addr = "127.0.0.1:7878".to_string();
+    let node_a_addr = next_addr();
 
-    let node_a = Arc::new(Node::new(node_a_addr.parse().unwrap()));
+    let node_a = Arc::new(Node::new(node_a_addr));
     let node_a_clone = Arc::clone(&node_a);
     spawn(async move { await!(node_a_clone.serve()).unwrap() });
 
     // Sleep to let node_a start its rpc server
     await!(sleep_ms(500));
 
-    let node_b = Node::new("127.0.0.1:7879".parse().unwrap());
-    await!(node_b.join(vec![node_a_addr.parse().unwrap()])).unwrap();
+    let node_b = Node::new(next_addr());
+    await!(node_b.join(vec![node_a_addr])).unwrap();
 
     let peers_a = await!(node_a.peers());
     assert_eq!(peers_a.len(), 1);
@@ -36,18 +43,18 @@ async fn join() {
 
 #[async_current_thread_test]
 async fn join_3_node() {
-    let node_a_addr = "127.0.0.1:7890".to_string();
-    let node_b_addr = "127.0.0.1:7891".to_string();
-    let node_c_addr = "127.0.0.1:7892".to_string();
+    let node_a_addr = next_addr();
+    let node_b_addr = next_addr();
+    let node_c_addr = next_addr();
 
-    let node_a = Node::new(node_a_addr.parse().unwrap());
+    let node_a = Node::new(node_a_addr);
     spawn(async move { await!(node_a.serve()).unwrap() });
 
-    let node_b = Arc::new(Node::new(node_b_addr.parse().unwrap()));
+    let node_b = Arc::new(Node::new(node_b_addr));
 
     await!(sleep_ms(500));
 
-    await!(node_b.join(vec![node_a_addr.parse().unwrap()])).unwrap();
+    await!(node_b.join(vec![node_a_addr])).unwrap();
 
     let node_b_clone = Arc::clone(&node_b);
     spawn(async move { await!(node_b_clone.serve()).unwrap() });
@@ -57,8 +64,8 @@ async fn join_3_node() {
 
     await!(sleep_ms(500));
 
-    let node_c = Node::new(node_c_addr.parse().unwrap());
-    await!(node_c.join(vec![node_a_addr.parse().unwrap()])).unwrap();
+    let node_c = Node::new(node_c_addr);
+    await!(node_c.join(vec![node_a_addr])).unwrap();
     spawn(async move { await!(node_c.serve()).unwrap() });
 
     await!(sleep_ms(1000));
@@ -82,4 +89,11 @@ where
     F: Future + Send + 'static,
 {
     TokioDefaultSpawner.spawn(f.map(|_| ())).unwrap();
+}
+
+static NEXT_PORT: AtomicUsize = AtomicUsize::new(1234);
+fn next_addr() -> SocketAddr {
+    use std::net::{IpAddr, Ipv4Addr};
+    let port = NEXT_PORT.fetch_add(1, Ordering::AcqRel) as u16;
+    SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port)
 }
